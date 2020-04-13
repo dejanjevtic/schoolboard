5abc3ced5e71240e4d73f53846afa2a1354c8244
Dejan Jevtić <dejan_d_jevtic@yahoo.com>
2020-04-13 20:03:57 +0200

    CSM Board

---
+++ class_CSM.php
@@ -0,0 +1,19 @@
+﻿<?php
+ 
+ class CSM extends Student{
+	 
+	private $result; 
+	 
+ 	public function __construct ($name, $grade) {
+		parent::__construct($name, $grade);
+		
+	}
+	
+	function pass(){
+				
+	}	
+ }
+
+
+ 
+?>
