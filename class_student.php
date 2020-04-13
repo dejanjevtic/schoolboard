c3b679dd6342ba17459ade27296bef7850e40e11
Dejan Jevtić <dejan_d_jevtic@yahoo.com>
2020-04-13 18:09:53 +0200

    db connect & Student class

---
+++ class_student.php
@@ -0,0 +1,19 @@
+﻿<?php
+ 
+ class Student {
+	 
+ 	private $name;	
+	public $grade;
+	
+	
+	function __construct($name, $grade) {
+		$this->name = $name;
+		$this->grade = $grade;
+	}
+	
+		
+ }
+
+
+ 
+?>
--- index.php
+++ index.php
@@ -7,6 +7,23 @@
 
 <body>
 
+<?php
+
+include 'class_student.php';
+include 'DB_connect.php';
+
+
+$url = $_SERVER['REQUEST_URI'];
+
+$parts=parse_url($url);
+$path_parts= explode('/', $parts['path']);
+echo $user = $path_parts[4];
+
+
+
+
+?>
+
 </body>
 
 </html>
